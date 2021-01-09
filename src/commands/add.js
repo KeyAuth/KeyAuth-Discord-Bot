582393a9e3071be3d0e87e25f44b55a3311d5c8e
mak6969 <74461188+mak6969@users.noreply.github.com>
2021-01-09 15:59:36 -0500

    Update add.js

--- src/commands/add.js
+++ src/commands/add.js
@@ -11,13 +11,14 @@ module.exports = {
     async run (client, message, args) {
 
     let days = parseInt(args[0])
+    let level = parseInt(args[1])
 
     let sellerkey = await db.get(`token_${message.guild.id}`)
     if(sellerkey === null) return message.channel.send(new Discord.MessageEmbed().setDescription(`The \`SellerKey\` **Has Not Been Set!**\n In Order To Use This Bot You Must Run The \`setseller\` Command First.`).setColor("RED").setTimestamp());
 
     if(!args[0]) return message.channel.send('Please Provide A Valid Expiry');
 
-    fetch(`https://keyauth.com/api/seller/?sellerkey=${sellerkey}&type=add&expiry=${days}`)
+    fetch(`https://keyauth.com/api/seller/?sellerkey=${sellerkey}&type=add&expiry=${days}&level=${level}`)
     .then(res => res.text())
     .then(text => {
     message.channel.send(new Discord.MessageEmbed().setTitle('Key Successfully Added!').addField('Key Add By:', message.author).addField('Key Added:', `${text}`).setColor("GREEN").setTimestamp());
