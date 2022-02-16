e30b512e2d7638c7b6d6de01024e2ae2b1bab105
William Nelson <83034852+wnelson03@users.noreply.github.com>
2022-02-16 15:14:33 -0500

    Update URL

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -13,7 +13,7 @@ module.exports = {
     let sellerkey = await db.get(`token_${message.guild.id}`)
     if(sellerkey === null) return message.channel.send(new Discord.MessageEmbed().setDescription(`The \`SellerKey\` **Has Not Been Set!**\n In Order To Use This Bot You Must Run The \`setseller\` Command First.`).setColor("RED").setTimestamp());
 
-    fetch(`https://keyauth.com/api/seller/?sellerkey=${sellerkey}&type=stats`)
+    fetch(`https://keyauth.win/api/seller/?sellerkey=${sellerkey}&type=stats`)
     .then(res => res.json())
     .then(json => {
     message.channel.send(new Discord.MessageEmbed().setTitle('Application Statistics').addField('Unused Keys:', `${json['unused']}`).addField('Used Keys:', `${json['used']}`).addField('Paused Keys:', `${json['paused']}`).addField('Banned Keys:', `${json['banned']}`).addField('Total Keys:', `${json['totalkeys']}`).addField('Webhooks:', `${json['webhooks']}`).addField('Files:', `${json['files']}`).addField('Vars:', `${json['vars']}`).addField('Reseller Accounts:', `${json['resellers']}`).addField('Manager Accounts:', `${json['managers']}`).addField('Total Accounts:', `${json['totalaccs']}`).setColor("BLUE").setTimestamp());
