691d6ed463265d0af73d19aca18053ca6d7b1320
William Nelson <83034852+wnelson03@users.noreply.github.com>
2022-02-16 15:04:50 -0500

    Update add.js

--- src/commands/add.js
+++ src/commands/add.js
@@ -62,7 +62,7 @@ let filter = m => m.author.id === message.author.id
           return message.channel.send(new Discord.MessageEmbed().setTitle('Failure, non-numerical answer provided.').setColor("RED"));
           }
 
-              fetch(`https://keyauth.com/api/seller/?sellerkey=${sellerkey}&type=add&expiry=${days}&level=${level}&amount=${amount}&format=text`)
+              fetch(`https://keyauth.win/api/seller/?sellerkey=${sellerkey}&type=add&expiry=${days}&level=${level}&amount=${amount}&format=text`)
     .then(res => res.text())
     .then(text => {
     message.channel.send(new Discord.MessageEmbed().setTitle('Key Successfully Added!').addField('Key Add By:', message.author).addField('Key Added:', `${text}`).setColor("GREEN").setTimestamp());
