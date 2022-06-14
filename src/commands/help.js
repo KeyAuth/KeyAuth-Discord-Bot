7857943857616a7ce913f8f6d5290873dfaf93fb
William Nelson <83034852+wnelson03@users.noreply.github.com>
2022-06-14 15:26:16 -0400

    Add help command

---
+++ src/commands/help.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const db = require('quick.db')
+const fetch = require('node-fetch')
+const Discord = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("help")
+        .setDescription("Help command for bot"),
+    async execute(interaction) {
+		interaction.editReply({ embeds: [new Discord.MessageEmbed().setTitle(`KeyAuth help menu`).addField('_ _', 'This bot is for the open-source authentication system [KeyAuth](https://keyauth.win)\n\nIf you\'re using the cloud hosted version of KeyAuth, you\'ll need the seller plan to use. You can test before purchase by using the demo seller account in the #demo-accounts channel of the [Discord server](https://discord.gg/keyauth)').addField('Source code:', `[https://github.com/KeyAuth/KeyAuth-Discord-Bot](https://github.com/KeyAuth/KeyAuth-Discord-Bot)`).addField('Library', 'Discord.js').addField('Tutorial video', '[https://www.youtube.com/watch?v=orQ_5BQCd-U](https://www.youtube.com/watch?v=orQ_5BQCd-U)').setColor("BLUE").setTimestamp()], ephemeral: true})
+    },
+};
