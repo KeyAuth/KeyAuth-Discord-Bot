6f448f1178def2deece5e6f6645299d2a5ab201b
mazkdevf <mazkdevf@gmail.com>
2022-07-18 02:11:59 +0300

    Forgot to Update These

--- src/commands/delsub.js
+++ src/commands/delsub.js
@@ -7,43 +7,41 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("deletesub")
         .setDescription("Delete user's subscription")
-		.addStringOption((option) => 
-        option
-            .setName("user")
-            .setDescription("Username of user you're deleting subscription from")
-            .setRequired(true)
+        .addStringOption((option) =>
+            option
+                .setName("user")
+                .setDescription("Username of user you're deleting subscription from")
+                .setRequired(true)
         )
-        .addStringOption((option) => 
-        option
-            .setName("name")
-            .setDescription("Name of subscription you're deleting from user")
-            .setRequired(true)
+        .addStringOption((option) =>
+            option
+                .setName("name")
+                .setDescription("Name of subscription you're deleting from user")
+                .setRequired(true)
         ),
     async execute(interaction) {
-		let idfrom = null;
-		
-		if(interaction.guild == null)
-			idfrom = interaction.user.id;
-		else
-			idfrom = interaction.guild.id;
-		
+        let idfrom = null;
+
+        if (interaction.guild == null)
+            idfrom = interaction.user.id;
+        else
+            idfrom = interaction.guild.id;
+
         let sellerkey = await db.get(`token_${idfrom}`)
-        if(sellerkey === null) return interaction.editReply({ embeds: [new Discord.EmbedBuilder().setDescription(`The \`SellerKey\` **Has Not Been Set!**\n In Order To Use This Bot You Must Run The \`setseller\` Command First.`).setColor(Colors.Red).setTimestamp()], ephemeral: true})
+        if (sellerkey === null) return interaction.editReply({ embeds: [new Discord.EmbedBuilder().setDescription(`The \`SellerKey\` **Has Not Been Set!**\n In Order To Use This Bot You Must Run The \`setseller\` Command First.`).setColor(Colors.Red).setTimestamp()], ephemeral: true })
 
         let user = interaction.options.getString("user")
         let name = interaction.options.getString("name")
 
         fetch(`https://keyauth.win/api/seller/?sellerkey=${sellerkey}&type=delsub&user=${user}&sub=${name}`)
-        .then(res => res.json())
-        .then(json => {
-        if(json.success)
-        {
-            interaction.editReply({ embeds: [new Discord.EmbedBuilder().setTitle(json.message).addFields('Subscription deleted:', `\`${name}\``).setColor(Colors.Green).setTimestamp()], ephemeral: true})
-        }
-        else
-        {
-            interaction.editReply({ embeds: [new Discord.EmbedBuilder().setTitle(json.message).addFields([{ name: 'Note:', value: `Your seller key is most likely invalid. Change your seller key with \`setseller\` command.`}]).setColor(Colors.Red).setTimestamp()], ephemeral: true})
-        }
-        })
+            .then(res => res.json())
+            .then(json => {
+                if (json.success) {
+                    interaction.editReply({ embeds: [new Discord.EmbedBuilder().setTitle(json.message).addFields([{ name: 'Subscription deleted:', value: `\`${name}\`` }]).setColor(Colors.Green).setTimestamp()], ephemeral: true })
+                }
+                else {
+                    interaction.editReply({ embeds: [new Discord.EmbedBuilder().setTitle(json.message).addFields([{ name: 'Note:', value: `Your seller key is most likely invalid. Change your seller key with \`setseller\` command.` }]).setColor(Colors.Red).setTimestamp()], ephemeral: true })
+                }
+            })
     },
 };
--- src/commands/userdata.js
+++ src/commands/userdata.js
@@ -7,39 +7,53 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("userdata")
         .setDescription("Retrieve info from a user")
-        .addStringOption((option) => 
-        option
-            .setName("user")
-            .setDescription("Specify user to lookup")
-            .setRequired(true)
+        .addStringOption((option) =>
+            option
+                .setName("user")
+                .setDescription("Specify user to lookup")
+                .setRequired(true)
         ),
     async execute(interaction) {
-		let idfrom = null;
-		
-		if(interaction.guild == null)
-			idfrom = interaction.user.id;
-		else
-			idfrom = interaction.guild.id;
-		
+        let idfrom = null;
+
+        if (interaction.guild == null)
+            idfrom = interaction.user.id;
+        else
+            idfrom = interaction.guild.id;
+
         let sellerkey = await db.get(`token_${idfrom}`)
-        if(sellerkey === null) return interaction.editReply({ embeds: [new Discord.EmbedBuilder().setDescription(`The \`SellerKey\` **Has Not Been Set!**\n In Order To Use This Bot You Must Run The \`setseller\` Command First.`).setColor(Colors.Red).setTimestamp()], ephemeral: true})
+        if (sellerkey === null) return interaction.editReply({ embeds: [new Discord.EmbedBuilder().setDescription(`The \`SellerKey\` **Has Not Been Set!**\n In Order To Use This Bot You Must Run The \`setseller\` Command First.`).setColor(Colors.Red).setTimestamp()], ephemeral: true })
 
         let user = interaction.options.getString("user")
 
         fetch(`https://keyauth.win/api/seller/?sellerkey=${sellerkey}&type=userdata&user=${user}`)
-        .then(res => res.json())
-        .then(json => {
-            if (!json.success) return interaction.editReply({ embeds: [new Discord.EmbedBuilder().setTitle(json.message).addFields([{ name: 'Note:', value: `Your seller key is most likely invalid. Change your seller key with \`/setseller\` command.`}]).setColor(Colors.Red).setFooter({ text: "KeyAuth Discord Bot" }).setTimestamp()], ephemeral: true})
-			let hwid = json.hwid ?? "N/A";
-			let ip = json.ip ?? "N/A";
-			let lastlogin = (json.lastlogin !== null && json.lastlogin !== undefined) ? `<t:${json.lastlogin}:f>` : "N/A";
-			let expiry = "N/A";
-			let subscription = "N/A";
-			if (json.subscriptions.length !== 0) {
-				expiry = (json.subscriptions[0].expiry !== null && json.subscriptions[0].expiry !== undefined) ? `<t:${json['subscriptions'][0]['expiry']}:f>` : "N/A";
-				subscription = (json.subscriptions[0].subscription !== null && json.subscriptions[0].subscription !== undefined) ? json.subscriptions[0].subscription : "N/A";
-			}
-			interaction.editReply({ embeds: [new Discord.EmbedBuilder().setTitle(`User data for ${user}`).addFields('Expiry:', `${expiry}`).addFields('Subscription name:', `${subscription}`).addFields('Last Login:', `${lastlogin}`).addFields('HWID:', `${hwid}`).addFields('Created On:', `<t:${json['createdate']}:f>`).addFields('IP Address:', `${ip}`).setColor(Colors.Blue).setTimestamp()], ephemeral: true})
-        })
+            .then(res => res.json())
+            .then(json => {
+                if (!json.success) return interaction.editReply({ embeds: [new Discord.EmbedBuilder().setTitle(json.message).addFields([{ name: 'Note:', value: `Your seller key is most likely invalid. Change your seller key with \`/setseller\` command.` }]).setColor(Colors.Red).setFooter({ text: "KeyAuth Discord Bot" }).setTimestamp()], ephemeral: true })
+                let hwid = json.hwid ?? "N/A";
+                let ip = json.ip ?? "N/A";
+                let lastlogin = (json.lastlogin !== null && json.lastlogin !== undefined) ? `<t:${json.lastlogin}:f>` : "N/A";
+                let expiry = "N/A";
+                let subscription = "N/A";
+                if (json.subscriptions.length !== 0) {
+                    expiry = (json.subscriptions[0].expiry !== null && json.subscriptions[0].expiry !== undefined) ? `<t:${json['subscriptions'][0]['expiry']}:f>` : "N/A";
+                    subscription = (json.subscriptions[0].subscription !== null && json.subscriptions[0].subscription !== undefined) ? json.subscriptions[0].subscription : "N/A";
+                }
+
+                const embed = new Discord.EmbedBuilder()
+                    .setTitle(`User data for ${user}`)
+                    .addFields([
+                        { name: 'Expiry:', value: `${expiry}` },
+                        { name: 'Subscription name:', value: `${subscription}` },
+                        { name: 'Last Login:', value: `${lastlogin}` },
+                        { name: 'HWID:', value: `${hwid}` },
+                        { name: 'Created On:', value: `<t:${json['createdate']}:f>` },
+                        { name: 'IP Address:', value: `${ip}` },
+                    ])
+                    .setColor(Colors.Blue)
+                    .setTimestamp()
+
+                interaction.editReply({ embeds: [embed], ephemeral: true })
+            })
     },
 };
